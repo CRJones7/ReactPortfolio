f0220548359bcf561c79b6e1dbe90e7f20935400
Courtney Jones <courtneyrjones95@gmail.com>
2020-08-19 19:35:01 -0500

    Updates

---
+++ precache-manifest.acfdcc778341c19985b50f356176c26b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2ec30940912f049bf1199cb7f773757",
+    "url": "/ReactPortfolio/index.html"
+  },
+  {
+    "revision": "9db212737403ec6913c1",
+    "url": "/ReactPortfolio/static/css/main.fffc8acc.chunk.css"
+  },
+  {
+    "revision": "0c53f78c2b1486218d07",
+    "url": "/ReactPortfolio/static/js/2.a989cf1c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ReactPortfolio/static/js/2.a989cf1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9db212737403ec6913c1",
+    "url": "/ReactPortfolio/static/js/main.8dcb90d5.chunk.js"
+  },
+  {
+    "revision": "245e88873ef0a2539a5e",
+    "url": "/ReactPortfolio/static/js/runtime-main.ebaa2495.js"
+  },
+  {
+    "revision": "5b50965dda3ac2e8997229d37ccf8a45",
+    "url": "/ReactPortfolio/static/media/BalooTamma2-Medium.5b50965d.ttf"
+  },
+  {
+    "revision": "77f7e699c5acc6598e9b7f079afcea23",
+    "url": "/ReactPortfolio/static/media/fittnesstracker.77f7e699.png"
+  },
+  {
+    "revision": "62a462108fb6082d1cdf193ead0b0519",
+    "url": "/ReactPortfolio/static/media/image.62a46210.jpg"
+  },
+  {
+    "revision": "9e79c422174a0e7f208c055386c23d8e",
+    "url": "/ReactPortfolio/static/media/resume.9e79c422.pdf"
+  },
+  {
+    "revision": "122fa7be6f046fc90da375f77d54c789",
+    "url": "/ReactPortfolio/static/media/scope.122fa7be.png"
+  },
+  {
+    "revision": "27021cf3ca826c19ca2f86f489e1dffd",
+    "url": "/ReactPortfolio/static/media/travelbuddy.27021cf3.png"
+  },
+  {
+    "revision": "f5d2bf648157b3d15cd5cadcad6df011",
+    "url": "/ReactPortfolio/static/media/weatherapp.f5d2bf64.png"
+  }
+]);
